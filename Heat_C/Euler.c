a527ce037199dd18bac09e9e56cdd497a7900ab3
Afifah Maya Iknaningrum <afifahmaya35@gmail.com>
2018-05-15 09:57:31 +0900

    kelas seat in

--- Heat_C/Euler.c
+++ Heat_C/Euler.c
@@ -4,21 +4,68 @@
 
 int main(int argc, char *argv[])
 {
-    int i, devider = 10;
-    double y, yy, h =1./devider;
+    int i;
+    double y, yy, h, t, er, k1, k2, k3, k4;
 
     FILE *euler;
-    FILE *error;
+    FILE *rk;
+    char filename1[30];
+    char filename2[30];
 
-    y = 0.04; //initial condition
-    euler = fopen("euler.txt", "w");
-    error = fopen("euler_er.txt","w");
-    fprintf(euler,"%.3f %.3f \n", 0., y);
-    fprintf(error,"%.3f %.3f \n", 0., y);
-    for(i=1;i<=devider;i++){
-        y = y + h*5*(2-y)*y;
-        fprintf(euler,"%.3f %.3f \n", h*i, y);
+    for(int dev=4;dev<=128;dev=dev*2)
+    {
+        h = 1./dev;
+        
+        sprintf(filename1, "euler_%.3d.txt",dev);
+        sprintf(filename2, "rk_%.3d.txt",dev);
+        
+        euler = fopen(filename1,"w");
+        rk = fopen(filename2, "w");
+
+        y = 0.04; //initial condition    
+        
+        fprintf(euler,"%.3f %.3f %.3f %.3f \n", 0., y, y, 0.);
+        fprintf(rk,"%.3f %.3f %.3f %.3f \n", 0., y, y, 0.);
+
+        //Using Euler
+        for(i=1;i<=dev;i++)
+        {
+            t=i*h;
+            y = 5*h*(2-y)*y + y;
+            yy = 2./(1+49*exp(-10*t));
+            er = fabs(yy-y);
+            fprintf(euler,"%.3f %.3f %.3f %.3f \n", t, y, yy, er);
+        }
+
+        //Using RK
+        y = 0.04; //initial condition
+        for(i=1;i<=dev;i++)
+        {
+            t=i*h;
+            k1 = 5*(2-y)*y;
+            k2 = 5*(2-(y+(k1*h/2.)))*(y+(k1*h/2.));
+            k3 = 5*(2-(y+(k2*h/2.)))*(y+(k2*h/2.));
+            k4 = 5*(2-(y+(k3*h)))*(y+(k3*h));
+            y = h*(k1 + 2*k2 + 2*k3 + k4)/6. + y;
+            yy = 2./(1+49*exp(-10*t));
+            er = fabs(yy-y);
+            fprintf(rk,"%.3f %.3f %.3f %.3f \n", t, y, yy, er);
+        }
+
+        //plot
+        FILE *pipe = popen("gnuplot", "w");
+        fprintf(pipe, "reset \n");
+        fprintf(pipe, "set terminal png \n");
+        fprintf(pipe, "set output 'plot%.3d.png'\n", dev);
+        fprintf(pipe, "set title 'Euler and Runge-Kutta for devider %.3d' \n", dev);
+        fprintf(pipe, "set xrange [0:1] \n");
+        fprintf(pipe, "set xlabel 't' \n");
+        fprintf(pipe, "set yrange [0:4] \n");
+        fprintf(pipe, "set ylabel 'y' \n");
+        fprintf(pipe, "plot '%s' using 1:2 w linespoint title 'Euler y numeric', '%s' using 1:2 w linespoint title 'RK y numeric', '%s' using 1:3 w linespoint title 'Exact y' , '%s' using 1:4 w linespoint title 'Euler differences' , '%s' using 1:4 w linespoint title 'RK differences'\n", filename1, filename2, filename1, filename1, filename2);
+        fclose(pipe);
     }
-    fclose(euler);
 
+    fclose(euler);
+    fclose(rk);
 }
