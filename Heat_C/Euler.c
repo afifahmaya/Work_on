ff08d05e08038556523d65807447b5a4875e172f
Afifah Maya Iknaningrum <afifahmaya35@gmail.com>
2018-05-15 18:31:58 +0900

    mac

--- Heat_C/Euler.c
+++ Heat_C/Euler.c
@@ -5,14 +5,17 @@
 int main(int argc, char *argv[])
 {
     int i;
-    double y, yy, h, t, er, k1, k2, k3, k4;
+    double y, yy, h, t, er, k1, k2, k3, k4, log_1, log_2;
 
     FILE *euler;
     FILE *rk;
+    FILE *er_log;
     char filename1[30];
     char filename2[30];
 
-    for(int dev=4;dev<=128;dev=dev*2)
+    er_log = fopen("log_error.txt","w");
+
+    for(int dev=128;dev>=4;dev=dev/2)
     {
         h = 1./dev;
         
@@ -22,11 +25,13 @@ int main(int argc, char *argv[])
         euler = fopen(filename1,"w");
         rk = fopen(filename2, "w");
 
-        y = 0.04; //initial condition    
+        y = 0.04; //initial condition  
+        log_1 = 0.;
+        log_2 = 0.;  
         
         fprintf(euler,"%.3f %.3f %.3f %.3f \n", 0., y, y, 0.);
         fprintf(rk,"%.3f %.3f %.3f %.3f \n", 0., y, y, 0.);
-
+        
         //Using Euler
         for(i=1;i<=dev;i++)
         {
@@ -35,6 +40,7 @@ int main(int argc, char *argv[])
             yy = 2./(1+49*exp(-10*t));
             er = fabs(yy-y);
             fprintf(euler,"%.3f %.3f %.3f %.3f \n", t, y, yy, er);
+            if(er>=log_1) log_1=er;
         }
 
         //Using RK
@@ -50,7 +56,10 @@ int main(int argc, char *argv[])
             yy = 2./(1+49*exp(-10*t));
             er = fabs(yy-y);
             fprintf(rk,"%.3f %.3f %.3f %.3f \n", t, y, yy, er);
+            if(er>=log_2) log_2=er;
         }
+
+        fprintf(er_log, "%.3f %.3f %.3f \n", h, log_1, log_2);
 
         //plot
         FILE *pipe = popen("gnuplot", "w");
@@ -66,6 +75,17 @@ int main(int argc, char *argv[])
         fclose(pipe);
     }
 
+    FILE *pipe1 = popen("gnuplot","w");
+    fprintf(pipe1, "reset \n");
+    fprintf(pipe1, "set term png \n");
+    fprintf(pipe1, "set logscale \n");
+    fprintf(pipe1, "set output 'log.png'\n");
+    fprintf(pipe1, "set xlabel '1/N' \n");
+    fprintf(pipe1, "set ylabel 'error' \n");
+    fprintf(pipe1, "plot '%s' using 1:2 w l title 'Euler', '%s' using 1:3 w l title 'Classical RK'\n","log_error.txt","log_error.txt");
+    fclose(pipe1);
+
     fclose(euler);
     fclose(rk);
+    fclose(er_log);
 }
